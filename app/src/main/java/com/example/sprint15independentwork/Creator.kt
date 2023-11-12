1ea8b4c3f759ebb8703c13b47a4044280b0d37ee
Babaylov7 <babaylov-ayu@mail.ru>
2023-11-12 21:00:09 +0500

    2

--- app/src/main/java/com/example/sprint15independentwork/Creator.kt
+++ app/src/main/java/com/example/sprint15independentwork/Creator.kt
@@ -1,10 +1,14 @@
 package com.example.sprint15independentwork
 
+import android.app.Activity
 import com.example.sprint15independentwork.data.MoviesRepositoryImpl
 import com.example.sprint15independentwork.data.network.RetrofitNetworkClient
 import com.example.sprint15independentwork.domain.api.MoviesInteractor
 import com.example.sprint15independentwork.domain.api.MoviesRepository
 import com.example.sprint15independentwork.domain.impl.MoviesInteractorImpl
+import com.example.sprint15independentwork.presentation.MoviesSearchController
+import com.example.sprint15independentwork.presentation.PosterController
+import com.example.sprint15independentwork.ui.movies.MoviesAdapter
 
 object Creator {
     private fun getMoviesRepository(): MoviesRepository{
@@ -13,5 +17,13 @@ object Creator {
 
     fun provideMoviesInteractor(): MoviesInteractor {
         return MoviesInteractorImpl(getMoviesRepository())
+    }
+
+    fun provideMoviesSearchController(activity: Activity, adapter: MoviesAdapter): MoviesSearchController {
+        return MoviesSearchController(activity, adapter)
+    }
+
+    fun providePosterController(activity: Activity): PosterController {
+        return PosterController(activity)
     }
 }
